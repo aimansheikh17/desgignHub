bacc5f14b8b48fb8b285eaecc2219492c2f604a3
aimansheikh17 <aimansheikh200117@gmail.com>
2025-10-11 15:29:16 +0530

    seccc901306

--- src/components/OurWorks.jsx
+++ src/components/OurWorks.jsx
@@ -3,6 +3,11 @@ import "../styles/OurWorks.css";
 import vmevalves from "../images/vmevalves0.png";
 import hkborwells from "../images/HK-BOREWELLS.png";
 import buynest from "../images/BuyNest.png";
+import vmeposter from "../images/poster1.png";
+import vmelogo from "../images/vmelogo.png";
+import vmeposter1 from "../images/poster2.png";
+import vmeposter3 from "../images/poster3.png";
+import vmeposter4 from "../images/poster4.png";
 
 const works = [
   {
@@ -19,6 +24,31 @@ const works = [
     title: "BuyNest",
     subtitle: "Responsive Website Development",
     image: buynest,
+  },
+  {
+    title: "VME Poster",
+    subtitle: "Poster Design",
+    image: vmeposter,
+  },
+  {
+    title: "VME",
+    subtitle: "Logo Design",
+    image: vmelogo,
+  },
+  {
+    title: "VME",
+    subtitle: "Poster Design",
+    image: vmeposter1,
+  },
+  {
+    title: "VME",
+    subtitle: "Poster Design",
+    image: vmeposter3,
+  },
+  {
+    title: "VME",
+    subtitle: "Poster Design",
+    image: vmeposter4,
   },
 ];
 
